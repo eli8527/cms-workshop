199d8a2bc56493944c14d87b86649467e21c7ffa
Eric Li <ericyoungli@gmail.com>
2020-03-31 15:47:43 -0400

    add image field

--- 1_kirby/site/templates/default.php
+++ 1_kirby/site/templates/default.php
@@ -1,3 +1,15 @@
 <?php snippet('header'); ?>
-  <h1><?= $page->title() ?></h1>
+  <div class="layout-wrapper">
+    <div class="columns">
+      <div class="column">
+        <img src="<?= $page->hero_image()->toFile()->url(); ?>">
+      </div>
+      <div class="column">
+        <h2><?= $page->title() ?></h2>
+        <div class="text">
+          <?= $page->main_content()->kirbytext(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
 <?php snippet('footer'); ?>
